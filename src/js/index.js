ba04194cc6567c6ff9e800170ea5d6a52a3a219e
Elisa Andrade <elisalvsan@gmail.com>
2023-10-01 21:05:54 -0300

    corrigindo erros

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,24 @@
-const botoes = document.querySelector(".botao");
-
+const botoes = document.querySelectorAll(".botao");
 const personagens = document.querySelectorAll(".personagem");
 
 botoes.forEach((botao, indice) => {
   botao.addEventListener("click", () => {
-    desselecionarBotoa();
-
-    botao.classList.add("selecionado");
-
+    desselecionarBotao();
     desselecionarPersonagem();
 
+    botao.classList.add("selecionado");
     personagens[indice].classList.add("selecionado");
   });
 });
-
-function desselecionarBotoa() {
-  const botaoSelecionado = document.querySelector(".botao.selecionado");
-  botaoSelecionado.classList.remove("selecionado");
-}
 
 function desselecionarPersonagem() {
   const personagemSelecionado = document.querySelector(
     ".personagem.selecionado"
   );
   personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+  const botaoSelecionado = document.querySelector(".botao.selecionado");
+  botaoSelecionado.classList.remove("selecionado");
 }
